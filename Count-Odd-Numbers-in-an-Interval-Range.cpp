de818b52141e7ec0675cecac742fc9e333334632
Nitin414 <aryannitin43@gmail.com>
2026-04-17 01:43:31 +0530

    [1523] [Time Beats: 71.76%] [Memory Beats: 70.09%] - LeetPush

--- Count-Odd-Numbers-in-an-Interval-Range.cpp
+++ Count-Odd-Numbers-in-an-Interval-Range.cpp
@@ -1,14 +1,19 @@
 1class Solution {
 2public:
 3    int countOdds(int low, int high) {
-4        int k=0;
-5        for(int i=low;i<=high;i++)
-6        {
-7            if(i%2!=0)
-8            {
-9                k++;
-10            }
-11        }
-12        return k;
-13    }
-14};
+4        int k;
+5        k=(high-low);
+6        if(k==1)
+7        {
+8            return 1;
+9        }
+10        else if(high%2==1||low%2==1)
+11        {
+12            return k/2+1;
+13        }
+14        else
+15        {
+16            return k/2;
+17        }
+18    }
+19};
