fc52c65e97ee0a376d4890b59402930eda8a6bdd
Nitin414 <aryannitin43@gmail.com>
2026-05-05 01:06:18 +0530

    [2540] [Time Beats: 100.00%] [Memory Beats: 82.51%] - LeetPush

--- Minimum-Common-Value.cpp
+++ Minimum-Common-Value.cpp
@@ -1,23 +1,22 @@
 1class Solution {
 2public:
 3    int getCommon(vector<int>& nums1, vector<int>& nums2) {
-4      int maxi =0,i=0,j=0;
-5      int minx=maxi;
-6      while(i<nums1.size() && j < nums2.size())
-7      {
-8        if(nums1[i]==nums2[j])
-9        {
-10           return nums1[i];
-11        }
-12        else if(nums1[i]>nums2[j])
-13        {
-14            j++;
-15        }
-16        else if(nums1[i]<nums2[j])
-17        {
-18            i++;
-19        }
-20      }
-21     return -1;
-22    }
-23};
+4      int i=0,j=0;
+5      while(i<nums1.size() && j < nums2.size())
+6      {
+7        if(nums1[i]==nums2[j])
+8        {
+9           return nums1[i];
+10        }
+11        else if(nums1[i]>nums2[j])
+12        {
+13            j++;
+14        }
+15        else if(nums1[i]<nums2[j])
+16        {
+17            i++;
+18        }
+19      }
+20     return -1;
+21    }
+22};
