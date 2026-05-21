16d41f45de072464b56f94fd5200d1069649edcc
Nitin414 <aryannitin43@gmail.com>
2026-05-22 02:27:05 +0530

    [75] [Time Beats: 100.00%] [Memory Beats: 47.23%] - LeetPush

--- Sort-Colors.cpp
+++ Sort-Colors.cpp
@@ -1,6 +1,15 @@
 1class Solution {
 2public:
 3    void sortColors(vector<int>& nums) {
-4        sort(nums.begin(),nums.end());
-5    }
-6};
+4        for(int i=0;i<nums.size();i++)
+5        {
+6            for(int j=i+1;j<nums.size();j++)
+7            {
+8                if(nums[i]>nums[j])
+9                {
+10                    swap(nums[i],nums[j]);
+11                }
+12            }
+13        }
+14    }
+15};
